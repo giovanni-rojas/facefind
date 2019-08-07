1baad710e7b8908f55792be2f459eebcc75ae183
giovanni-rojas <gio.rojas15@gmail.com>
2019-08-06 22:27:19 -0700

    added sick particle background, detecting string input and button clicks on detect button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
-{//       <FacialRecognition />
+const particleOptions = {
+  particles: {  
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
 }
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles 
+          className='particles'
+          params={ particleOptions }
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={ this.onInputChange } 
+          onButtonSubmit={ this.onButtonSubmit } />
+        
+        {//<FacialRecognition /> 
+        }
+
+      </div>
+    );
+  }
 }
 
 export default App;
