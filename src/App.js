89b82a1a0d087d0ca32d8e531aab1f61f5950fc9
giovanni-rojas <gio.rojas15@gmail.com>
2019-09-12 20:48:15 -0700

    entry count correctly updating through PUT method

--- src/App.js
+++ src/App.js
@@ -86,13 +86,30 @@ class App extends Component {
     this.setState({input: event.target.value});
   }
 
-  onButtonSubmit = () => {
+  onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input })
     app.models
       .predict(
         Clarifai.FACE_DETECT_MODEL, 
         this.state.input)      //wouldn't work with this.state.imageUrl (idk why)
-      .then(response => this.displayFaceBox(this.calculateBoxLocation(response)))    //answer: setState is asynchronous, so React hadn't finished updating imageUrl's state. Can fix w/ a callback setState(updater, callback)function(response) {     //from https://www.clarifai.com/models/face-detection-image-recognition-model-a403429f2ddf4b49b307e318f00e528b-detection
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3001/image', 
+                  {
+                    method: 'put',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify(
+                    {
+                      id: this.state.user.id
+                    })
+                  })
+                    .then(response => response.json())
+                    .then(count => {
+                      this.setState(Object.assign(this.state.user, { entries: count }))
+                    })
+        }
+        this.displayFaceBox(this.calculateBoxLocation(response))    //answer: setState is asynchronous, so React hadn't finished updating imageUrl's state. Can fix w/ a callback setState(updater, callback)function(response) {     //from https://www.clarifai.com/models/face-detection-image-recognition-model-a403429f2ddf4b49b307e318f00e528b-detection
+      })
       .catch(err => console.log(err));
   }
 
@@ -121,7 +138,7 @@ class App extends Component {
               <Rank name={ this.state.user.name } entries={ this.state.user.entries }/>
               <ImageLinkForm 
                 onInputChange={ this.onInputChange } 
-                onButtonSubmit={ this.onButtonSubmit } />
+                onButtonSubmit={ this.onPictureSubmit } />
               <FacialRecognition box={ box } imageUrl={ imageUrl }/>
             </div> 
           : (
