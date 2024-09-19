a62ab4c46272022be3dd6649fc8ba3588f042a08
Gio Rojas <gio.rojas15@gmail.com>
2024-09-19 15:40:13 -0700

    Allow Enter keypress

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './ImageLinkForm.css';
 
 const ImageLinkForm = ( { onInputChange, onButtonSubmit } ) => {
+
+	const handleKeyPress = (event) => {
+		if (event.key === 'Enter') {
+			onButtonSubmit();
+		}
+	}
+
 	return(
 		<div>
 			<p className='f3'>
@@ -9,7 +16,7 @@ const ImageLinkForm = ( { onInputChange, onButtonSubmit } ) => {
 			</p>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
-					<input className='f4 pa2 w-70 center' type='text' onChange={ onInputChange }/>
+					<input className='f4 pa2 w-70 center' type='text' onChange={ onInputChange } onKeyPress={handleKeyPress}/>
 					<button 
 						className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
 						onClick = { onButtonSubmit }
